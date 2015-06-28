e938badb8f824380b3176c05431d2a5015dc9961
Administrator <Administrator@SKY-20150625FZA>
2015-06-29 00:00:41 +0800

    tree

---
+++ java-base/src/main/java/org/tony/base/java/data/structures/tree/Node.java
@@ -0,0 +1,14 @@
+package org.tony.base.java.data.structures.tree;
+
+public class Node {
+
+	/* 数据域的key. */
+	int key;
+	/* 数据. */
+	double data;
+	/* 左子树. */
+	Node leftNode;
+	/* 右子树. */
+	Node rightNode;
+
+}
---
+++ java-base/src/main/java/org/tony/base/java/data/structures/tree/Tree.java
@@ -0,0 +1,30 @@
+package org.tony.base.java.data.structures.tree;
+
+public class Tree {
+	/* 根节点. */
+	private Node root;
+
+	public void find(int key) {
+		if(root ==null){
+			return;
+		}
+		Node current = root;
+		
+		while(current!=null){
+			int key0 = current.key;
+			if(key>key0){
+				
+			}
+		}
+
+	}
+
+	public void insert(int key, double value) {
+
+	}
+
+	public void delete(int key) {
+
+	}
+
+}
