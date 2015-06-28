ce37a443375b2953e193457f2234b46d8f3f99b2
a <a@a-PC>
2015-06-28 13:30:58 +0800

    java-base sort-->insert

---
+++ java-base/src/main/java/org/tony/base/java/App.java
@@ -0,0 +1,13 @@
+package org.tony.base.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ java-base/src/main/java/org/tony/base/java/sort/InsertSort.java
@@ -0,0 +1,5 @@
+package org.tony.base.java.sort;
+
+public class InsertSort {
+
+}
